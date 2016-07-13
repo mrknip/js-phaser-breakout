e805b82c7ca9b7e2eafbd850bcffdf69eeb6c689
Drew Johnson <drew.johnson.dev@gmail.com>
2016-07-13 13:20:45 +0100

    Working blocks, scoring and acceleration

---
+++ js/config.js
@@ -0,0 +1,59 @@
+var gameProperties = {
+  gameHeight: 500,
+  gameWidth: 400,
+
+  paddleHeight: 50,
+  paddleSpeed: 400,
+  paddleSegments: 6,
+  paddleSegWidth: 10, // Make this un-magic
+  paddleAngleMultiplier: -15,
+
+  ballRestartDelay: 2,
+  ballSpeed: 400,
+  ballAngles: [45, 60, 120, 135],
+
+  blockBase: 180,
+  blockWidth: 50,
+  blockRows: ['red', 'orange', 'green', 'yellow'],
+
+  scores: {
+    'yellow': 1,
+    'green' : 3,
+    'orange': 5,
+       'red': 7
+  },
+
+  scoreDisplayX: 50,
+  scoreDisplayY: 45,
+  livesDisplayX: 350,
+  livesDisplayY: 45,
+
+  numberDisplayStyle: {
+    font: '40px Orbitron',
+    fontWeight: 'bold',
+    fill: '#FFFFFF',
+    align: 'center'
+  },
+}
+
+var imageAssets = {
+  paddleURL: 'assets/paddle.png',
+  paddleName: 'paddle',
+
+  ballURL: 'assets/ball.png',
+  ballName: 'ball',
+
+  blockURL: {
+    'yellow': 'assets/block_yellow.png',
+    'green': 'assets/block_green.png',
+    'orange': 'assets/block_orange.png',
+    'red': 'assets/block_red.png'
+  },
+
+  blockName: {
+    'yellow': 'blockYellow',
+    'green': 'blockGreen',
+    'orange': 'blockOrange',
+    'red': 'blockRed'
+  }
+}
--- js/game.js
+++ js/game.js
@@ -1,28 +1,3 @@
-var gameProperties = {
-  gameHeight: 600,
-  gameWidth: 400,
-
-  paddleHeight: 50,
-  paddleSpeed: 400,
-  paddleSegments: 6,
-  paddleSegWidth: 10, // Make this un-magic
-  paddleAngleMultiplier: -15,
-
-  ballSpeed: 500
-}
-
-var imageAssets = {
-  paddleURL: 'assets/paddle.png',
-  paddleName: 'paddle',
-
-  ballURL: 'assets/ball.png',
-  ballName: 'ball',
-
-  blockURL: 'assets/block_blue.png',
-  blockName: 'blockBlue'
-}
-
-
 var mainState = function(game) {
   this.paddleSprite;
 
@@ -30,14 +5,21 @@ var mainState = function(game) {
   this.paddleRightKey;
 
   this.blockGroup;
-  this.blockBlue;
+  this.score;
+  this.lives;
+
+  this.scoreDisplay;
+  this.livesDisplay;
 }
 
 mainState.prototype = {
   preload: function () {
     game.load.image(imageAssets.paddleName, imageAssets.paddleURL);
     game.load.image(imageAssets.ballName, imageAssets.ballURL);
-    game.load.image(imageAssets.blockName, imageAssets.blockURL);
+    
+    gameProperties.blockRows.forEach(function(colour){
+      game.load.image(imageAssets.blockName[colour], imageAssets.blockURL[colour])
+    });
   },
 
   create: function () {
@@ -45,29 +27,15 @@ mainState.prototype = {
     this.initControls();
     this.initPhysics();
 
-    this.startLevel();
+    this.startDemo();
   },
 
   update: function () {
     this.movePaddle();
 
-    game.physics.arcade.overlap(this.ball, this.paddleSprite, this.ballPaddleCollide);
-    game.physics.arcade.collide(this.ball, this.blockGroup, this.blockHit);
+    game.physics.arcade.collide(this.ball, this.paddleSprite, this.ballPaddleCollide, null, this);
+    game.physics.arcade.collide(this.ball, this.blockGroup, this.blockHit, null, this);
   },
-
-  movePaddle: function () {
-    if (this.paddleLeftKey.isDown) 
-    {
-      this.paddleSprite.body.velocity.x = -gameProperties.paddleSpeed;
-    } 
-    else if (this.paddleRightKey.isDown) 
-    {
-      this.paddleSprite.body.velocity.x = gameProperties.paddleSpeed;
-    } else {
-      this.paddleSprite.body.velocity.x = 0;
-    }
-  },
-
 
   // ==========================
   // Init functions
@@ -81,6 +49,12 @@ mainState.prototype = {
     this.ball.anchor.set(0.5, 0.5);
 
     this.blockGroup = game.add.group();
+
+    this.scoreDisplay = game.add.text(gameProperties.scoreDisplayX, gameProperties.scoreDisplayY, "0", gameProperties.numberDisplayStyle);
+    this.scoreDisplay.anchor.set(0, 0.5);
+
+    this.livesDisplay = game.add.text(gameProperties.livesDisplayX, gameProperties.livesDisplayY, "5", gameProperties.numberDisplayStyle);
+    this.livesDisplay.anchor.set(1, 0.5);
   },
 
   initPhysics: function () {
@@ -95,12 +69,11 @@ mainState.prototype = {
 
     this.ball.body.bounce.set(1, 1);
     this.ball.body.collideWorldBounds = true;
+    this.ball.checkWorldBounds = true;
+    this.ball.events.onOutOfBounds.add(this.ballOutOfBounds, this);
 
     this.blockGroup.physicsBodyType = Phaser.Physics.ARCADE;
     this.blockGroup.enableBody = true; 
-
-
-    console.log(this.blockGroup);
 
   },
 
@@ -109,21 +82,80 @@ mainState.prototype = {
     this.paddleRightKey = game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
   },
 
-  startLevel: function () {
-    for (var i = 0; i < 12; i++) {
-      this.blockGroup.create((i * 50), 300, imageAssets.blockName)
+  // =========================
+  // Controller functions
+  // =========================
+
+  movePaddle: function () {
+    if (this.paddleLeftKey.isDown) 
+    {
+      this.paddleSprite.body.velocity.x = -gameProperties.paddleSpeed;
+    } 
+    else if (this.paddleRightKey.isDown) 
+    {
+      this.paddleSprite.body.velocity.x = gameProperties.paddleSpeed;
+    } else {
+      this.paddleSprite.body.velocity.x = 0;
+    }
+  },
+
+  addBlocks: function () {
+    var rowHeight = gameProperties.blockBase,
+        numBlocks = Math.floor(gameProperties.gameWidth / gameProperties.blockWidth);
+
+    for (var i = gameProperties.blockRows.length - 1; i >= 0; i--) {
+      for (var j = 0; j < numBlocks; j++) {
+        var b = game.add.sprite((j * gameProperties.blockWidth), rowHeight, imageAssets.blockName[gameProperties.blockRows[i]])
+        this.blockGroup.add(b);
+        b.body.immovable = true;
+        b.colour = gameProperties.blockRows[i];
+      }
+      rowHeight -=15;
+
+      for (var j = 0; j < numBlocks; j++) {
+        var b = game.add.sprite((j * gameProperties.blockWidth), rowHeight, imageAssets.blockName[gameProperties.blockRows[i]])
+        this.blockGroup.add(b);
+        b.body.immovable = true;
+        b.colour = gameProperties.blockRows[i];
+      }
+      rowHeight -=15;
     }
 
     this.blockGroup.setAll('body.immovable', true);
+  },
 
+  startDemo: function () {
+    this.resetBall();
+    this.disablePit(true);
+    this.game.input.onDown.add(this.startGame, this);
+  },
+
+  startGame: function () {
+    this.game.input.onDown.remove(this.startGame, this);
+
+    this.addBlocks();
+    this.disablePit(false);
+    this.resetScore();
     this.resetBall();
   },
 
+  resetScore: function () {
+    this.score = 0;
+    this.lives = 5;
+  },
+
   resetBall: function () {
+    this.ball.speed = gameProperties.ballSpeed;
+    this.ball.bounces = 0;
     this.ball.reset(game.rnd.between(50, 350), 400);
 
-    this.ball.body.velocity.x = gameProperties.ballSpeed;
-    this.ball.body.velocity.y = gameProperties.ballSpeed;
+    game.physics.arcade.velocityFromAngle(game.rnd.pick(gameProperties.ballAngles), 
+                                          this.ball.speed,
+                                          this.ball.body.velocity);
+  },
+
+  disablePit: function(bool) {
+    game.physics.arcade.checkCollision.down = bool;
   },
 
   ballPaddleCollide: function (ball, paddle) {
@@ -146,12 +178,35 @@ mainState.prototype = {
       returnAngle = -(70 - Math.abs(returnAngle));
     }
 
-    game.physics.arcade.velocityFromAngle(returnAngle, gameProperties.ballSpeed, ball.body.velocity);
+    game.physics.arcade.velocityFromAngle(returnAngle, this.ball.speed, this.ball.body.velocity);
+
+    this.ball.bounces++
+    if (this.ball.bounces == 12) { this.ball.speed += 100; }
+  },
+
+  ballOutOfBounds: function () {
+    console.log('out!')
+    this.lives--
+    this.updateLives();
+    this.resetBall();
   },
 
   blockHit: function (ball, block) {
-    console.log('hit')
     block.kill();
+    this.score += gameProperties.scores[block.colour];
+    this.updateScore();
+    
+    if (block.colour == 'orange') {
+      this.ball.speed += 20;
+    }
+  },
+
+  updateScore: function () {
+    this.scoreDisplay.setText(this.score);
+  },
+
+  updateLives: function () {
+    this.livesDisplay.setText(this.lives);
   }
 
 };
